60902f6670d6f44da49d8d8d889fbb4af6422aa7
zijiancogito <345981440@qq.com>
2023-09-11 23:47:19 +0800

    update

--- src/data/datagen.py
+++ src/data/datagen.py
@@ -56,11 +56,12 @@ def batch(count, save_dir, proc=28, level=3):
         os.makedirs(save_dir)
     config = Config()
     pool = multiprocessing.Pool(processes=proc)
-    for i in range(count):
-    # args = [(config, os.path.join(save_dir, f"{i}.c")) for i in range(count)]
-        save_path = os.path.join(save_dir, f"{i}.c")
+    # for i in range(count):
+    args = [(config, os.path.join(save_dir, f"{i}.c")) for i in range(count)]
+    pool.map(generator, args, 1000)
+        # save_path = os.path.join(save_dir, f"{i}.c")
         #print(save_path)
-        pool.apply_async(func=generator, args=(config, save_path))
+        # pool.apply_async(func=generator, args=(config, save_path))
         # generator(config, save_path)
 
     pool.close()
