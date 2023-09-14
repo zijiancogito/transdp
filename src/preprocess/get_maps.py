494f799966633a4545e0cfc2926cd63e10440fd4
zijiancogito <345981440@qq.com>
2023-09-14 10:56:33 +0800

    update

--- src/preprocess/get_maps.py
+++ src/preprocess/get_maps.py
@@ -34,10 +34,9 @@ def process_file(arch, ir, ir_dir, bin_dir, src_dir, asm_list_dir, src_list_dir,
     _, line_map, _ = elf_parser.parse_dwarf(bin_p)
     print(line_map)
 
-    maps = src_map.map_src_vs_asm(src_list_p, asm_p, line_map)
-    print(maps)
     import pdb
     pdb.set_trace()
+    maps = src_map.map_src_vs_asm(src_list_p, asm_p, line_map)
     # map_asm = os.path.join(map_dir, f"{bname}.asm")
     # map_src = os.path.join(map_dir, f"{bname}.c")
     # asm_f = open(map_asm, 'w')
